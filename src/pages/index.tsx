b6a105255c8c102bba2acbb2e7aaac7f2cae2575
serhatbek <serhat.bekk@gmail.com>
2024-02-08 16:04:12 +0300

    add layout page

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as Layout } from './Layout/Layout';
+export { default as Home } from './Home/Home';
+export { default as Courses } from './Courses/Courses';
+export { default as Events } from './Events/Events';
+export { default as ContactUs } from './ContactUs/ContactUs';
