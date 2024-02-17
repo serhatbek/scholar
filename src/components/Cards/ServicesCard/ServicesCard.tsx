65cad20e33644009a95de1179b00cc8eaee221af
serhatbek <serhat.bekk@gmail.com>
2024-02-17 23:50:09 +0300

    services card structure

---
+++ src/components/Cards/ServicesCard/ServicesCard.tsx
@@ -0,0 +1,38 @@
+import { Button } from '../../../components';
+import './ServicesCard.scss';
+
+interface ServicesCardProps {
+  item: itemProps;
+}
+
+interface itemProps {
+  title: string;
+  desc: string;
+  img: string;
+  btn: {
+    text: string;
+    url: string;
+    variant: string;
+    icon: string;
+  };
+}
+
+const ServicesCard = ({ item }: ServicesCardProps) => {
+  const { title, desc, img, btn } = item;
+  return (
+    <div className='services-card'>
+      <figure className='services-card__img'>
+        <img src={img} alt='' />
+      </figure>
+      <div className='services-card__body'>
+        <h4>{title}</h4>
+        <p>{desc}</p>
+        <Button href={btn.url} variant={btn.variant} iconRight={btn.icon}>
+          {btn.text}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesCard;
