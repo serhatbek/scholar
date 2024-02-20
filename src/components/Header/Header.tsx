f54343ac90b034f02028b13d1c81eb05e49a38e9
serhatbek <serhat.bekk@gmail.com>
2024-02-20 15:58:12 +0300

    close mobile menu when clicked outside

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,25 @@
 import './Header.scss';
-import { useEffect, useId, useState } from 'react';
+import { useEffect, useId, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { dataHeader } from '../../assets/data/dataHeader';
 import { Icon } from '../../components';
 import classNames from 'classnames';
+import handleClickOutside from '../../utils/handleClickOutside.js';
 
 const Header = () => {
   const { navLinks, registerBtn } = dataHeader;
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isSticky, setIsSticky] = useState<boolean>(false);
+  const menuRef = useRef(null);
 
-  const handleMobileMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const openMobileMenu = () => {
+    setIsMenuOpen(true);
   };
+  const closeMobileMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  handleClickOutside(menuRef, closeMobileMenu);
 
   useEffect(() => {
     const handleStickyHeader = () => {
@@ -40,6 +47,7 @@ const Header = () => {
         </div>
 
         <nav
+          ref={menuRef}
           className={classNames(
             `${isMenuOpen ? 'header__nav header__nav--visible' : 'header__nav'}`
           )}
@@ -62,7 +70,10 @@ const Header = () => {
           </ul>
         </nav>
 
-        <button className='btn header__action' onClick={handleMobileMenu}>
+        <button
+          className='btn header__action'
+          onClick={isMenuOpen ? closeMobileMenu : openMobileMenu}
+        >
           <Icon icon={isMenuOpen ? 'menu-close' : 'menu-open'} size='34' />
         </button>
       </div>
