7c2a6049eed4ba163e45721963df89d4c1052363
serhatbek <serhat.bekk@gmail.com>
2024-02-21 15:03:46 +0300

    testimonial card structure

---
+++ src/components/Cards/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,32 @@
+import './TestimonialCard.scss';
+
+interface TestimonialCardProps {
+  item: itemProps;
+}
+
+interface itemProps {
+  text: string;
+  img: string;
+  job: string;
+  name: string;
+}
+
+const TestimonialCard = ({ item }: TestimonialCardProps) => {
+  const { text, img, job, name } = item;
+  return (
+    <div className='testimonial__card'>
+      <p>{text}</p>
+      <div>
+        <figure>
+          <img src={img} alt='' />
+        </figure>
+        <div>
+          <span>{job}</span>
+          <h4>{name}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,3 +1,4 @@
 export { default as BannerCard } from './BannerCard/BannerCard';
 export { default as ServicesCard } from './ServicesCard/ServicesCard';
 export { default as TeamCard } from './TeamCard/TeamCard';
+export { default as TestimonialCard } from './TestimonialCard/TestimonialCard';
