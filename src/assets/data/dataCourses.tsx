9ed012c820437c81726dd766ad5c39a9662395d1
serhatbek <serhat.bekk@gmail.com>
2024-02-22 22:00:38 +0300

    add courses page data

--- src/assets/data/dataCourses.tsx
+++ src/assets/data/dataCourses.tsx
@@ -1,3 +1,10 @@
+const courseImg1 = 'images/course-01.jpg';
+const courseImg2 = 'images/course-02.jpg';
+const courseImg3 = 'images/course-03.jpg';
+const courseImg4 = 'images/course-04.jpg';
+const courseImg5 = 'images/course-05.jpg';
+const courseImg6 = 'images/course-06.jpg';
+
 export const dataCourses = {
   heading: {
     subtitle: 'COURSES',
@@ -11,6 +18,57 @@ export const dataCourses = {
     {
       label: 'Courses',
       url: '#',
+    },
+  ],
+  filterBtnList: ['Show All', 'Webdesign', 'Development', 'Wordpress'],
+  cards: [
+    {
+      href: '#',
+      img: courseImg1,
+      category: 'Webdesign',
+      price: '160',
+      teacher: 'Frank Knight',
+      desc: 'Learn Web Design',
+    },
+    {
+      href: '#',
+      img: courseImg2,
+      category: 'Development',
+      price: '340',
+      teacher: 'Darrell Mills',
+      desc: 'Web Development Tips',
+    },
+    {
+      href: '#',
+      img: courseImg3,
+      category: 'Wordpress',
+      price: '640',
+      teacher: 'Sadie Norman',
+      desc: 'Latest Web Trends',
+    },
+    {
+      href: '#',
+      img: courseImg4,
+      category: 'Development',
+      price: '450',
+      teacher: 'Frances Abbott',
+      desc: 'Online Learning Steps',
+    },
+    {
+      href: '#',
+      img: courseImg5,
+      category: 'Webdesign',
+      price: '240',
+      teacher: 'Billy Jackson',
+      desc: 'Full Stack Developer',
+    },
+    {
+      href: '#',
+      img: courseImg6,
+      category: 'Wordpress',
+      price: '320',
+      teacher: 'Donald Saunders',
+      desc: 'Be a WordPress Master',
     },
   ],
 };
