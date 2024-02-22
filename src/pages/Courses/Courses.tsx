f1cae775764164a2436e84a43e2334b5655c7317
serhatbek <serhat.bekk@gmail.com>
2024-02-23 01:15:19 +0300

    filter buttons styling

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,11 +1,21 @@
+import './Courses.scss';
 import { dataCourses } from '../../assets/data/dataCourses';
 import { Banner } from '../../components';
 
 const Courses = () => {
-  const { dataBreadcrumb, heading } = dataCourses;
+  const { dataBreadcrumb, heading, filterBtnList } = dataCourses;
   return (
     <>
       <Banner heading={heading} items={dataBreadcrumb} />
+      <section className='event-filter'>
+        <div className='container'>
+          <div className='event-filter__action'>
+            {filterBtnList?.map((item) => {
+              return <button>{item}</button>;
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
