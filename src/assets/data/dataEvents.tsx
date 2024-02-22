8a65242e8b8d5f5a01b27680e55d467bd3e4b189
serhatbek <serhat.bekk@gmail.com>
2024-02-22 11:55:52 +0300

    add event cards data

--- src/assets/data/dataEvents.tsx
+++ src/assets/data/dataEvents.tsx
@@ -1,3 +1,7 @@
+const event1 = 'images/event-01.jpg';
+const event2 = 'images/event-02.jpg';
+const event3 = 'images/event-03.jpg';
+
 export const dataEvents = {
   bannerTitle: 'Events',
   dataBreadcrumb: [
@@ -13,5 +17,73 @@ export const dataEvents = {
   dataSchedule: {
     subtitle: 'SCHEDULE',
     title: 'Upcoming Events',
+    cards: [
+      {
+        img: event1,
+        category: 'WEB DESIGN',
+        title: 'UI Best Practices',
+        details: [
+          {
+            title: 'Date:',
+            desc: '16 Feb 2036',
+          },
+          {
+            title: 'Duration:',
+            desc: '22 Hours',
+          },
+          {
+            title: 'Price:',
+            desc: '$120',
+          },
+        ],
+        btn: {
+          url: '/scholar/',
+        },
+      },
+      {
+        img: event2,
+        category: 'FRONT END',
+        title: 'New Design Trend',
+        details: [
+          {
+            title: 'Date:',
+            desc: '24 Feb 2036',
+          },
+          {
+            title: 'Duration:',
+            desc: '30 Hours',
+          },
+          {
+            title: 'Price:',
+            desc: '$320',
+          },
+        ],
+        btn: {
+          url: '/scholar/',
+        },
+      },
+      {
+        img: event3,
+        category: 'FULL STACK',
+        title: 'Web Programming',
+        details: [
+          {
+            title: 'Date:',
+            desc: '12 Mar 2036',
+          },
+          {
+            title: 'Duration:',
+            desc: '48 Hours',
+          },
+          {
+            title: 'Price:',
+            desc: '$440',
+          },
+        ],
+        btn: {
+          url: '/scholar/',
+        },
+      },
+    ],
   },
 };
---
+++ src/components/Cards/EventCard/EventCard.tsx
@@ -0,0 +1,13 @@
+import './EventCard.scss';
+
+const EventCard = () => {
+  return (
+    <div className='event-card'>
+      <figure className='event-card__img'></figure>
+      <div className='event-card__category'></div>
+      <div className='event-card__detail'></div>
+    </div>
+  );
+};
+
+export default EventCard;
