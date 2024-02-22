c90b7ffafe5cb293ab2c2a151816ab9238a5032d
serhatbek <serhat.bekk@gmail.com>
2024-02-22 12:02:35 +0300

    event card structure

--- src/components/Cards/EventCard/EventCard.tsx
+++ src/components/Cards/EventCard/EventCard.tsx
@@ -1,11 +1,50 @@
+import { Icon, Tag } from '../../../components';
 import './EventCard.scss';
 
-const EventCard = () => {
+interface EventCardProps {
+  item: itemProps;
+}
+
+interface itemProps {
+  img: string;
+  category: string;
+  title: string;
+  details: {
+    title: string;
+    desc: string;
+  }[];
+  btn: {
+    url: string;
+  };
+}
+
+const EventCard = ({ item }: EventCardProps) => {
+  const { img, category, title, details, btn } = item;
   return (
     <div className='event-card'>
-      <figure className='event-card__img'></figure>
-      <div className='event-card__category'></div>
-      <div className='event-card__detail'></div>
+      <figure className='event-card__img'>
+        <img src={img} alt='' />
+      </figure>
+      <div className='event-card__category'>
+        <Tag text={category} variant='white' />
+        <h4>{title}</h4>
+      </div>
+      <div className='event-card__detail'>
+        <ul>
+          {details?.map((item) => {
+            const { title, desc } = item;
+            return (
+              <>
+                <h6>{title}</h6>
+                <p>{desc}</p>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+      <a href={btn.url} className='event-card__action'>
+        <Icon icon='chevron-right' size='20' />
+      </a>
     </div>
   );
 };
